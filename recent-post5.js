3a7a3042b599abf8d7b9fe7cc74e6f5a7644486f
rawmastr <31912222+rawmastr@users.noreply.github.com>
2020-05-18 04:05:48 +0300

    Update and rename recent-post5.js to lazyload.js

--- recent-post5.js
+++
@@ -1,6 +0,0 @@
-&lt;script&gt;
-
-document.write(&quot;&lt;script src=\&quot;/feeds/posts/default/-/News?orderby=updated&amp;alt=json-in-script&amp;callback=labelthumbs\&quot;&gt;&lt;\/script&gt;&quot;);
-&lt;/script&gt;
-
-
