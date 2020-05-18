abad4a5813319a35964cb85743d2ecd99a1c1ac8
rawmastr <31912222+rawmastr@users.noreply.github.com>
2020-05-18 03:39:46 +0300

    Update recent-post5.js

--- recent-post5.js
+++ recent-post5.js
@@ -1,2 +1,6 @@
-document.write(&quot;&lt;script src=\&quot;/feeds/posts/default/-/Nasional?orderby=updated&amp;alt=json-in-script&amp;callback=labelthumbs\&quot;&gt;&lt;\/script&gt;&quot;);
+&lt;script&gt;
+
+document.write(&quot;&lt;script src=\&quot;/feeds/posts/default/-/News?orderby=updated&amp;alt=json-in-script&amp;callback=labelthumbs\&quot;&gt;&lt;\/script&gt;&quot;);
 &lt;/script&gt;
+
+
